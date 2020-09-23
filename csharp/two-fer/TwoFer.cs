fc1ebf37a7ca9ea740c8fb2607bd50817b59cdea
Andreas Heil <info@aheil.de>
2020-09-23 15:26:34 +0200

    two more exercises solved

---
+++ csharp/two-fer/TwoFer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+public static class TwoFer
+{
+    private const string PARAM_TEXT = "One for {0}, one for me.";
+
+    // In order to get the tests running, first you need to make sure the Speak method 
+    // can be called both without any arguments and also by passing one string argument.
+
+    public static string Speak(string name = "you")
+    {
+        return String.Format(PARAM_TEXT, name);
+    }
+}

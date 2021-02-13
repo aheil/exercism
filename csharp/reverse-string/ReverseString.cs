d700853cb729cb309b03a5f415c77f872d3803e9
Andreas Heil <info@aheil.de>
2021-02-13 22:49:20 +0100

    ReverseString exercise added

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        var a = input.ToCharArray();
+        Array.Reverse(a);
+        return new string(a);
+    }
+}

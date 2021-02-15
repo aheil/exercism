06caa75dd214a080b3c4f434f36a064209f91d90
Andreas Heil <info@aheil.de>
2021-02-15 23:27:44 +0100

    SumOfMultiples exercise added

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        HashSet<int> uniqueMultiplies = new HashSet<int>();
+
+        foreach (var m in multiples)
+        {
+            if (m == 0)
+                continue;
+
+            for (int i = 1; i * m < max; i++)
+            {
+                uniqueMultiplies.Add(i * m);
+            }
+        }
+
+        return uniqueMultiplies.Sum();
+    }
+}
